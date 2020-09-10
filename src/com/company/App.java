d08edcb75aafaede644ea3e7ab80f03b3d5f475f
pupil <pupil@ivkhk.ee>
2020-09-10 10:32:42 +0300

    Работающая программа с random и с attempt

--- src/com/company/App.java
+++ src/com/company/App.java
@@ -6,15 +6,27 @@ import java.util.Scanner;
 public class App {
     public void run() {
         System.out.println("Привет.");
-        System.out.print("Программа загадала число, угадай: ");
+        System.out.print("Программа загадала число от 0 до 5, угадай: ");
         Random random = new Random();
+        int attempt = 0;
+        do {
+
+
         int myNumber = random.nextInt(5 - 0 + 1) + 0;
         Scanner scanner = new Scanner(System.in);
         int userNumber = scanner.nextInt();
-        if (myNumber == userNumber)
+        if (myNumber == userNumber) {
             System.out.println("Ты угадал, загаданное число: " + myNumber);
-        else
-            System.out.println("Ты не угадал, загаданное число: " + myNumber);
-
+            break;
+        }else {
+            if (attempt > 2) {
+                System.out.println("Ты не угадал, загаданное число: " + myNumber);
+                break;
+            } else {
+                System.out.print("Попробуй ещё раз: ");
+            }
+            attempt++;
+            }
+        }while (true);
     }
 }
