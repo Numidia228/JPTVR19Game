311ab374fed3e3823948a5e43abe2c66c4d5a868
pupil <pupil@ivkhk.ee>
2020-09-10 11:15:00 +0300

    Работающая программа с пользовательским вводом

--- src/com/company/App.java
+++ src/com/company/App.java
@@ -6,27 +6,33 @@ import java.util.Scanner;
 public class App {
     public void run() {
         System.out.println("Привет.");
-        System.out.print("Программа загадала число от 0 до 5, угадай: ");
+        System.out.println("Загадйте минимальное и максимальное число...");
         Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Минимальное: ");
+        int userNumber1 = scanner.nextInt();
+        System.out.print("Максимальное: ");
+        int userNumber2 = scanner.nextInt();
+        System.out.print("Программа загадала число от " + userNumber1 + " до " + userNumber2 + ", угадай: ");
         int attempt = 0;
         do {
 
-
-        int myNumber = random.nextInt(5 - 0 + 1) + 0;
-        Scanner scanner = new Scanner(System.in);
-        int userNumber = scanner.nextInt();
-        if (myNumber == userNumber) {
-            System.out.println("Ты угадал, загаданное число: " + myNumber);
-            break;
-        }else {
-            if (attempt > 2) {
-                System.out.println("Ты не угадал, загаданное число: " + myNumber);
+            int myNumber = random.nextInt((userNumber2 - userNumber1) + 1) + userNumber1;
+            System.out.println(myNumber);
+            System.out.print("");
+            int userNumber = scanner.nextInt();
+            if (myNumber == userNumber) {
+                System.out.println("Ты угадал, загаданное число: " + myNumber);
                 break;
-            } else {
-                System.out.print("Попробуй ещё раз: ");
-            }
-            attempt++;
-            }
+            }else {
+                if (attempt > 2) {
+                    System.out.println("Ты не угадал, загаданное число: " + myNumber);
+                    break;
+                } else {
+                    System.out.print("Попробуй ещё раз: ");
+                }
+                attempt++;
+                }
         }while (true);
     }
 }
